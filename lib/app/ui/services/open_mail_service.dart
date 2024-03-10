c8ae2c38d632dc89fa94bf5bef142cf6d1edb832
boginni <brunnomarqueszr1@gmail.com>
2024-03-10 14:27:56 -0300

    feat: setup open mail service

---
+++ lib/app/ui/services/open_mail_service.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:open_mail_app/open_mail_app.dart';
+import 'package:ws_work_test_mobile/app/ui/extensions/context_extensions.dart';
+
+Future<void> openMailApp(BuildContext context) async {
+  var result = await OpenMailApp.openMailApp();
+
+  if (!context.mounted) {
+    return;
+  }
+
+  final bool cannotOpen = result.didOpen == false && result.canOpen == false;
+
+  if (cannotOpen) {
+    await _showNoMailAppsDialog(context);
+    return;
+  }
+
+  await showDialog(
+    context: context,
+    builder: (_) {
+      return MailAppPickerDialog(
+        mailApps: result.options,
+      );
+    },
+  );
+}
+
+Future<void> _showNoMailAppsDialog(BuildContext context) async {
+  await showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(context.appLocalizations.open_mail_app),
+        content: Text(context.appLocalizations.no_mail_app_installed),
+        actions: <Widget>[
+          FilledButton(
+            child: Text(context.appLocalizations.ok),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      );
+    },
+  );
+}
