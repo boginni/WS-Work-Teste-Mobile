bb14b5a6144f2a5f108f0c7eb5ae672d2c472d1c
boginni <brunnomarqueszr1@gmail.com>
2024-03-10 13:00:46 -0300

    feat: add app translation with l10n

--- lib/app/ui/app_widget.dart
+++ lib/app/ui/app_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:ws_work_test_mobile/app/ui/app_module.dart';
 import 'package:ws_work_test_mobile/app/ui/theme/ws_work_test_mobile_theme.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class AppWidget extends StatefulWidget {
   const AppWidget({super.key});
@@ -13,12 +15,25 @@ class AppWidget extends StatefulWidget {
 class _AppWidgetState extends State<AppWidget> {
   @override
   Widget build(BuildContext context) {
+    var local = WidgetsBinding.instance.platformDispatcher.locale;
+
+    const supportedLocales = [
+      ...AppLocalizations.supportedLocales,
+      Locale('pt', 'BR'),
+      Locale('en', 'US'),
+      Locale('fr', 'FR'),
+      Locale('en', ''),
+    ];
+
+
     final theme = WsWorkTestMobileTheme();
 
     Modular.setInitialRoute(AppModule.splash);
 
     return MaterialApp.router(
       title: 'Ws Work Test Mobile',
+      locale: local,
+      supportedLocales: supportedLocales,
       theme: theme.getLightThemeData(context),
       darkTheme: theme.getDarkThemeData(context),
       routerConfig: Modular.routerConfig,
