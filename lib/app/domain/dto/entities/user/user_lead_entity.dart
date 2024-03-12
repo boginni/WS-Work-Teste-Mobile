124a5c9e6f0afc11f49399a7173d08b25ac88b20
boginni <brunnomarqueszr1@gmail.com>
2024-03-12 08:51:51 -0300

    feat: setup user leads entity

---
+++ lib/app/domain/dto/entities/user/user_lead_entity.dart
@@ -0,0 +1,70 @@
+import 'package:ws_work_test_mobile/app/domain/dto/entities/entity.dart';
+
+import '../vehicle/vehicle_entity.dart';
+
+class UserLeadEntity extends Entity {
+//           id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         user_id INTEGER NOT NULL,
+//         vehicle_id INTEGER NOT NULL,
+//         sync_status BOOLEAN NOT NULL DEFAULT 0,
+//         sync_date TEXT,
+//         created_at TEXT NOT NULL,
+//         deleted_at TEXT
+
+  final int id;
+  final int userId;
+  final int vehicleId;
+  final bool syncStatus;
+  final DateTime? syncDate;
+  final DateTime createdAt;
+  final DateTime? deletedAt;
+
+  final String vehicleName;
+  final num vehiclePrice;
+
+  UserLeadEntity({
+    required this.id,
+    required this.userId,
+    required this.vehicleId,
+    required this.syncStatus,
+    required this.vehicleName,
+    required this.vehiclePrice,
+    required this.syncDate,
+    required this.createdAt,
+    required this.deletedAt,
+
+  });
+
+  factory UserLeadEntity.fromJson(Map<String, dynamic> json) {
+    return UserLeadEntity(
+      id: json['id'],
+      userId: json['user_id'],
+      vehicleId: json['vehicle_id'],
+      syncStatus: json['sync_status'],
+      // syncDate: json['sync_date'],
+      // createdAt: json['created_at'],
+      // deletedAt: json['deleted_at'],
+      syncDate: DateTime.parse(json['sync_date']),
+      createdAt: DateTime.parse(json['created_at']),
+      deletedAt: json['deleted_at'] != null ? DateTime.parse(json['deleted_at']) : null,
+      vehicleName: json['vehicle_name'],
+      vehiclePrice: json['vehicle_price'],
+    );
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'user_id': userId,
+      'vehicle_id': vehicleId,
+      'sync_status': syncStatus,
+      'vehicle_name': vehicleName,
+      'vehicle_price': vehiclePrice,
+      'sync_date': syncDate?.toIso8601String(),
+      'created_at': createdAt.toIso8601String(),
+      'deleted_at': deletedAt?.toIso8601String(),
+    };
+  }
+
+}
